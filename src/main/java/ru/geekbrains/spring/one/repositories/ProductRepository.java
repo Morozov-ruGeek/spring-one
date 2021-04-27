264bb30a359340696d7aeefa4e53e5ed188d3275
MorozovGeeK <morozov4811@gmail.com>
2021-04-27 16:44:54 +0400

    AMorozov homework 6. Unworked.

--- src/main/java/ru/geekbrains/spring/one/repositories/ProductRepository.java
+++ src/main/java/ru/geekbrains/spring/one/repositories/ProductRepository.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.geekbrains.spring.one.model.Category;
 import ru.geekbrains.spring.one.model.Product;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Optional;
 public class ProductRepository {
     private List<Product> products;
     private static SessionFactory factory;
+    private Category category;
 
     @Autowired
     public ProductRepository(SessionFactory factory){
